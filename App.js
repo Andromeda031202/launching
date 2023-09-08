525af6705325eca05092e5188188e83037e2cee3
Andromeda031202 <138282227+Andromeda031202@users.noreply.github.com>
2023-09-09 00:18:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+
+
+
+function App ()
+{
+
+
+return(
+  
+  <div className="landingtop">
+<header>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">CODE WITH US</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">HOME</a></li>
+      <li><a href="#">SUPPORT US</a></li>
+      <li><a href="#">FEATURES</a></li>
+      <li><a href="#">MODE</a></li>
+    </ul>
+  </div>
+</nav> 
+</header>
+  <h1 className="mainheading">CODE WITH US!</h1>
+        <pre className="para">
+          We want to express our immense gratitude and heartfelt thanks towards 
+          you for choosing our website as your medium of knowledge acquirement.
+          Hope we stand true to your expectations. Have a good day!
+        </pre>
+   <button className="secondhead" type="button" HREF="https://paytm.com/">BOOK NOW</button>
+   
+   <div className="info">
+    <h3 className="about">ABOUT US?!</h3>
+    <pre className="para2">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+    </pre>
+   </div>
+
+   <div className="contact">
+    <h3 className="contact">CONTACT US :</h3>
+    <a href = "https://www.facebook.com/" target = "_blank"><img SRC = "fb.png" ALT = "Facebook" style="float: right;"></img></a>
+    <a href = "https://mail.google.com/mail/u/0/" target = "_blank"> <img SRC = "gmail.jpg" ALT = "G-Mail" style="float: right;"></img></a>
+    <a href = "chrome://newtab/" target = "_blank"> <img SRC = "google.jpg" ALT = "Google" style="float: right;"></img></a>
+    <a href = "https://web.whatsapp.com/" target = "_blank"> <img SRC = "wap.jpg" ALT = "Whatsapp" style="float: right;"></img></a>
+    </div>
+    
+    
+   
+  </div>
+ 
+);     
+} 
+
+
+export default App;
---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App.js';
+
+ReactDOM.render(
+  <React.StrictMode> // Optional, you may remove this if you want
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
